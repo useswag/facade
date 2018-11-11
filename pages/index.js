b57b626057d2c4c51f013b3ec6eade0617b883d1
James Canning <james@brudil.com>
2018-11-11 10:52:15 +0000

    99754106633f94d350db34d548d6091a

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,27 @@ const LogoImg = styled('img')({
     borderRadius: '50%',
 });
 
+const BigDataImage = styled('img')({
+    width: '100vw',
+    height: '180vh',
+});
+
 const Tag = styled('h2')({
     fontStyle: 'italic',
+});
+
+const Button = styled('a')({
+    display: 'block',
+    padding: '1rem',
+    background: 'white',
+    color: COLORS.AccentBlue,
+    textDecoration: 'none',
+    fontSize: 30
+});
+
+const Actions = styled('div')({
+    display: 'flex',
+    justifyContent: 'space-around',
 });
 
 export default () => (
@@ -25,7 +44,12 @@ export default () => (
                 <LogoImg src="/static/logo.png" />
                 <h1>Welcome to Swag</h1>
                 <Tag>Banking for the justifiably paranoid.</Tag>
+                <Actions>
+                    <Button href="https://dbe559e8.ngrok.io/login">Sign in</Button>
+                    <Button href="https://m.me/useswagbank/">Log up</Button>
+                </Actions>
             </Container>
         </HeroSlice>
+        <BigDataImage src="/static/important.png" />
     </Main>
 );
